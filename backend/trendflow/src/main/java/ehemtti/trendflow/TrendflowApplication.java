e0e3ba3d14c170623bc67389e3cd9a7a2b15e195
bbookng <bbookng@gmail.com>
2023-03-06 17:46:57 +0900

    🔧 프로젝트 생성

---
+++ backend/trendflow/src/main/java/ehemtti/trendflow/TrendflowApplication.java
@@ -0,0 +1,13 @@
+package ehemtti.trendflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendflowApplication.class, args);
+	}
+
+}
