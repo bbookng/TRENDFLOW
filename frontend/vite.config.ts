8bbc545bbecaffd57ffafed7608cbd7af91087b3
kamoo2 <kamoon2da@gmail.com>
2023-03-09 07:49:20 +0900

    front project init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+    ],
+  },
+});
