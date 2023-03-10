74242a876c97660560102611b166ec00e6cec8d7
tjrghks96 <tjrghks96@naver.com>
2023-03-10 13:32:58 +0900

    Feature : react-router-dom v6 설치 후 router 설정 완료

---
+++ frontend/src/constants/path.ts
@@ -0,0 +1,10 @@
+export const ROUTER_PATH = {
+  LANDING_PAGE: 'landing',
+  SOCIAL_MAIN_PAGE: 'social',
+  SOCIAL_RESULT_PAGE: 'social/result',
+  YOUTUBUE_MAIN_PAGE: 'youtube',
+  YOUTUBUE_RESULT_PAGE: 'youtube/result',
+  COMPARISON_MAIN_PAGE: 'comparison',
+  COMPARISON_RESULT_PAGE: 'comparison/result',
+  NOT_FOUND_PAGE: 'error',
+};
