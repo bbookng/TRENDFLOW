90f8a8018b46cbfb8da3c9386e43fbe986699e0b
bangbongbim <dkdpdl12311@gmail.com>
2023-03-14 17:51:43 +0900

    Feature :  Divider 다크모드

--- frontend/src/styles/emotion.d.ts
+++ frontend/src/styles/emotion.d.ts
@@ -5,9 +5,10 @@ type ThemeId = 'light' | 'dark';
 declare module '@emotion/react' {
   export interface Theme {
     [key in ThemeId]: {
+      contentBackground: string;
       background: string;
-      color: string;
-      primary: string;
+      positive: string;
+      negative: string;
     };
   }
 }
