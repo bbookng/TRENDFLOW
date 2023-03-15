02804ae71f152c706c703254ec3d56b7a74a4de1
김수민 <liijj@naver.com>
2023-03-15 09:20:36 +0900

    style: Typography props 수정

--- frontend/src/components/atoms/Typography/index.styles.ts
+++ frontend/src/components/atoms/Typography/index.styles.ts
@@ -1,15 +1,9 @@
-/* eslint-disable consistent-return */
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { TypographyPropsInterface } from '@/components/atoms/Typography';
 import { FONT_SIZE } from '@/constants/styles';
 
-const typographyStyle = (props: TypographyPropsInterface) => css`
-  font-size: ${props.variant ? FONT_SIZE[props.variant] : ''};
-  color: ${props.color};
-  font-weight: ${props.weight};
-`;
-
-export const Typography = styled.div`
-  ${typographyStyle}
+export const Typography = styled.div<Partial<TypographyPropsInterface>>`
+  font-size: ${({ variant }) => FONT_SIZE[variant!] || ''};
+  color: ${({ theme, color }) => color || theme.text};
+  font-weight: ${({ weight }) => weight || '400'};
 `;
