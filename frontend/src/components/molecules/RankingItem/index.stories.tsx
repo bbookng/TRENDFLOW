962475a2c4dc8686efabdee018ec07aa33357785
김수민 <liijj@naver.com>
2023-03-16 11:31:01 +0900

    Feature : RankingItem Storybook

---
+++ frontend/src/components/molecules/RankingItem/index.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import RankingItem from '@/components/molecules/RankingItem';
+
+export default {
+  title: 'Molecules/RankingItem',
+  component: RankingItem,
+  argTypes: {
+    type: {
+      options: ['up', 'down', 'new', 'same'],
+      control: { type: 'radio' },
+    },
+    rank: {
+      control: { type: 'text' },
+    },
+    keyword: {
+      control: { type: 'text' },
+    },
+    step: {
+      control: { type: 'text' },
+    },
+    width: {
+      control: { type: 'text' },
+    },
+    margin: {
+      control: { type: 'text' },
+    },
+  },
+} as ComponentMeta<typeof RankingItem>;
+
+const Template: ComponentStory<typeof RankingItem> = (args) => <RankingItem {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  type: 'up',
+  rank: '1',
+  keyword: '스타벅스',
+  step: '2',
+  width: '200px',
+  margin: '0 0 0.5rem 0',
+};
