507dad125e0bce538ebda0e39f19c5e0e439fd91
김수민 <liijj@naver.com>
2023-03-17 17:48:43 +0900

    Feature : atoms, molecules index files add

---
+++ frontend/src/components/atoms/index.tsx
@@ -0,0 +1,10 @@
+import Badge from '@/components/atoms/Badge';
+import Button from '@/components/atoms/Button';
+import Divider from '@/components/atoms/Divider';
+import Img from '@/components/atoms/Img';
+import Input from '@/components/atoms/Input';
+import Label from '@/components/atoms/Label';
+import Paper from '@/components/atoms/Paper';
+import Typography from '@/components/atoms/Typography';
+
+export { Badge, Button, Divider, Img, Input, Label, Paper, Typography };
