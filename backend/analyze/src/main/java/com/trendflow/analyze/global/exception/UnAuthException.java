801d036b081b88989315eb466b05df6963b3bf91
tableMinPark <tablemin_park@daum.net>
2023-03-20 19:59:32 +0900

    NewBranch : Analyze Branch MSA 기초 설정

---
+++ backend/analyze/src/main/java/com/trendflow/analyze/global/exception/UnAuthException.java
@@ -0,0 +1,7 @@
+package com.trendflow.analyze.global.exception;
+
+import com.trendflow.analyze.global.code.BasicCode;
+
+public class UnAuthException extends RuntimeException {
+    BasicCode code;
+}
