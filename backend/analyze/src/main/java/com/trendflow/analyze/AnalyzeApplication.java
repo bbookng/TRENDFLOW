861b520e120b3b9c2c1617c0b7337a9decddd423
tablemin_park <tablemin_park@daum.net>
2023-03-20 11:23:14 +0900

    NewBranch : analyze init commit

---
+++ backend/analyze/src/main/java/com/trendflow/analyze/AnalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.trendflow.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzeApplication.class, args);
+	}
+
+}
