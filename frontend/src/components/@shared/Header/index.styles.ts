6d393dfa51c5af94d38cea5cde0be2572c762741
tjrghks96 <tjrghks96@naver.com>
2023-03-21 15:32:41 +0900

    Style : isDark 전역 상태에 따른 style 수정

--- frontend/src/components/@shared/Header/index.styles.ts
+++ frontend/src/components/@shared/Header/index.styles.ts
@@ -3,20 +3,25 @@ import { MEDIA_QUERY } from '@/constants/media';
 import { PALETTE } from '@/constants/palette';
 
 export const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 56px;
   border-bottom: 0.5px solid ${PALETTE.WHITE400};
-  background-color: transparent;
+  background-color: ${({ theme }) => theme.background};
   padding: 0 2rem;
   z-index: 1000;
 
   @media ${MEDIA_QUERY.DESKTOP} {
     display: none;
   }
+`;
+
+export const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
