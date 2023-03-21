a87251ad3a92e5c88066939e307c631cbd58488c
tjrghks96 <tjrghks96@naver.com>
2023-03-21 15:31:02 +0900

    Feature : isDark 전역 상태 store 생성, toggle action 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import BaseLayout from '@/Layout/BaseLayout';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { darkTheme, lightTheme } from '@/styles/theme';
+import router from '@/router';
+import GlobalStyle from '@/styles/GlobalStyle';
+import { useAppSelector } from '@/hooks/storeHook';
 
 const App = () => {
+  const { isDark } = useAppSelector((state) => state);
   return (
-    <BaseLayout>
-      <Outlet />
-    </BaseLayout>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 };
 
