673f56ec993aa5b6f8fa76d7774f2dcad8effaa2
tjrghks96 <tjrghks96@naver.com>
2023-03-23 12:05:01 +0900

    Feature : Redux Login 상태에 따른 NavUser 버튼

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import themeReducer from '@/store/slices/themeSlice';
 import testReducer from '@/store/slices/testSlice';
 import navbarReducer from '@/store/slices/navbarSlice';
+import loginReducer from '@/store/slices/loginSlice';
 
 const rootReducer = combineReducers({
   isDark: themeReducer,
   test: testReducer,
   isNavbar: navbarReducer,
+  isLogin: loginReducer,
 });
 
 export default rootReducer;
