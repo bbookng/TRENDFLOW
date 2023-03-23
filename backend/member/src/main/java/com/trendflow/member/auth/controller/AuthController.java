92bedf6c10a8a841f5288cd89afa4eda12ab1485
tablemin_park <tablemin_park@daum.net>
2023-03-23 17:37:07 +0900

    Feature : authController 매핑 수정

--- backend/member/src/main/java/com/trendflow/member/auth/controller/AuthController.java
+++ backend/member/src/main/java/com/trendflow/member/auth/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member/auth")
+@RequestMapping("/member")
 public class AuthController {
 
     private final AuthService authService;
