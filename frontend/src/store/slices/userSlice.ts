29921c9728f64f501a9dc9b06bcccb0bb26d03b6
김수민 <liijj@naver.com>
2023-03-24 10:19:20 +0900

    Fix : 카카오 로그인 수정

--- frontend/src/store/slices/userSlice.ts
+++ frontend/src/store/slices/userSlice.ts
@@ -12,19 +12,16 @@ interface UserStateInterface {
   userName: string;
 }
 
-interface UserResponseInterface {
-  name: string;
-  accessToken: string;
-  refreshToken: string;
-}
-
 const initialState: UserStateInterface = {
   isLoggedIn: false,
   userName: '',
 };
 
+const { VITE_MAIN: MAIN } = import.meta.env;
+
 export const login = createAsyncThunk('user/login', async (data: LoginDataInterface) => {
   const res = await api.post('/member/login', data);
+  console.log(res.data);
   return res.data;
 });
 
@@ -36,8 +33,9 @@ export const userSlice = createSlice({
     builder.addCase(login.fulfilled, (state, { payload }) => {
       state.isLoggedIn = true;
       state.userName = payload.name;
-      window.localStorage.setItem('accessToken', payload.accessToken);
       document.cookie = `refreshToken=${payload.refreshToken}`;
+      window.localStorage.setItem('accessToken', payload.accessToken);
+      window.location.href = MAIN;
     });
   },
 });
