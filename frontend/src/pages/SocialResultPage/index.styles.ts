8c047b48e1c4e21891964991bf81c986249d7fe8
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-03-27 00:28:10 +0900

    FE-feature / 소셜 결과 페이지 제작중

--- frontend/src/pages/SocialResultPage/index.styles.ts
+++ frontend/src/pages/SocialResultPage/index.styles.ts
@@ -1,1 +1,18 @@
 import styled from '@emotion/styled';
+import { PALETTE } from '@/constants/palette';
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TypeWrapper = styled.div`
+  width: 50%;
+`;
+
+export const HighLight = styled.span`
+  color: ${PALETTE.BRAND400};
+  font-weight: bold;
+`;
--- frontend/src/pages/SocialResultPage/index.tsx
+++ frontend/src/pages/SocialResultPage/index.tsx
@@ -1,7 +1,18 @@
+import { Typography } from '@/components/atoms';
 import { SearchBar } from '@/components/molecules';
+import * as S from './index.styles';
 
 const SocialResultPage = () => {
-  return <SearchBar />;
+  return (
+    <S.TitleWrapper>
+      <S.TypeWrapper>
+        <Typography variant="H2">
+          <S.HighLight>삼성전자</S.HighLight> 소셜 분석 리포트
+        </Typography>
+      </S.TypeWrapper>
+      <SearchBar />
+    </S.TitleWrapper>
+  );
 };
 
 export default SocialResultPage;
