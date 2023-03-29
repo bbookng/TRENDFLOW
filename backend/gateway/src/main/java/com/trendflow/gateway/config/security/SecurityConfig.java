40b50b359e596bd3108d3736808853c428d5072c
박상민 <tablemin_park@daum.net>
2023-03-29 16:53:14 +0900

    Fix : Spring Security 설정

--- backend/gateway/src/main/java/com/trendflow/gateway/config/security/SecurityConfig.java
+++ backend/gateway/src/main/java/com/trendflow/gateway/config/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.trendflow.gateway.config.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+
+                .anyRequest().permitAll()
+
+                .and()
+                .logout().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable();
+
+        return http.build();
+    }
+
 }
