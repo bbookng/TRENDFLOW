fc88ea8306fb436f8f17de424eaefd2b19319d64
tjrghks96 <tjrghks96@naver.com>
2023-04-04 16:07:55 +0900

    Feature : RelatedContent 목업 설정 완료

--- frontend/src/mocks/data/index.tsx
+++ frontend/src/mocks/data/index.tsx
@@ -6,5 +6,20 @@ import pComment from '@/mocks/data/analyze/pComment.json';
 import recommend from '@/mocks/data/keyword/recommend.json';
 import wordCloud from '@/mocks/data/keyword/wordCloud.json';
 import bookmark from '@/mocks/data/member/bookmark.json';
+import relatedArticle from '@/mocks/data/analyze/relatedArticle.json';
+import relatedBlog from '@/mocks/data/analyze/relatedBlog.json';
+import relatedYoutube from '@/mocks/data/analyze/relatedYoutube.json';
 
-export { social, hot, relate, youtube, pComment, recommend, wordCloud, bookmark };
+export {
+  social,
+  hot,
+  relate,
+  youtube,
+  pComment,
+  recommend,
+  wordCloud,
+  bookmark,
+  relatedArticle,
+  relatedBlog,
+  relatedYoutube,
+};
