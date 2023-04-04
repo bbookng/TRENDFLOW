116d8f7681b771aafbf525d33ee52ce4f8c50731
김수민 <liijj@naver.com>
2023-04-04 17:59:26 +0900

    Feature : ComparisonLineChart 구현 및 api 연결

--- frontend/src/types/comparison.ts
+++ frontend/src/types/comparison.ts
@@ -1,22 +1,14 @@
+export interface ComparisionAnalysisItemInterface {
+  date: string;
+  keyword1: number;
+  keyword2: number;
+  type: string;
+  difference: number;
+}
+
 export interface ComparisonAnalysisInterface {
-  grapeQuotientCompare: [
-    {
-      date: string;
-      keyword1: number;
-      keyword2: number;
-      type: string;
-      difference: number;
-    }
-  ];
-  mentionCountCompare: [
-    {
-      date: string;
-      keyword1: number;
-      keyword2: number;
-      type: string;
-      difference: number;
-    }
-  ];
+  grapeQuotientCompare: Array<ComparisionAnalysisItemInterface>;
+  mentionCountCompare: Array<ComparisionAnalysisItemInterface>;
 }
 
 export interface ComaparisonReqBodyInterface {
