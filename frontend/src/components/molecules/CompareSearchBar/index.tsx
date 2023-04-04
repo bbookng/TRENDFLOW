9dd94e721bfa20938410291407354b8445ffca82
김수민 <liijj@naver.com>
2023-04-04 15:43:15 +0900

    Feature: Search Bar React-hook-form 적용, 공백 입력 시 툴팁

--- frontend/src/components/molecules/CompareSearchBar/index.tsx
+++ frontend/src/components/molecules/CompareSearchBar/index.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import * as S from './index.styles';
+import { Tooltip } from 'react-tooltip';
 import { ROUTER_PATH } from '@/constants/path';
+import 'react-tooltip/dist/react-tooltip.css';
+import * as S from './index.styles';
 
 interface FormDataInterface {
   keyword1: string;
@@ -9,7 +11,11 @@ interface FormDataInterface {
 }
 
 const CompareSearchBar = ({ keywords }: { keywords?: Array<string> }) => {
-  const { register, handleSubmit } = useForm<FormDataInterface>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataInterface>();
   const navi = useNavigate();
 
   const onValid = ({ keyword1, keyword2 }: FormDataInterface) => {
@@ -20,19 +26,38 @@ const CompareSearchBar = ({ keywords }: { keywords?: Array<string> }) => {
     <S.Form onSubmit={handleSubmit(onValid)}>
       <S.FormInputWrapper>
         <S.FormInput
-          {...register('keyword1', { required: '첫 번째 키워드를 입력하세요.' })}
+          {...register('keyword1', {
+            required: '첫 번째 키워드를 입력하세요.',
+            // 공백 검사
+            validate: { noSpace: (v) => !/\s/.test(v) },
+          })}
           defaultValue={keywords && keywords[0]}
           placeholder="첫 번째 키워드"
+          // 툴팁 설정
+          data-tooltip-id="keyword1"
+          data-tooltip-content="공백은 입력할 수 없습니다."
+          data-tooltip-place="bottom"
         />
         <S.FormInput
-          {...register('keyword2', { required: '두 번째 키워드를 입력하세요.' })}
+          {...register('keyword2', {
+            required: '두 번째 키워드를 입력하세요.',
+            // 공백 검사
+            validate: { noSpace: (v) => !/\s/.test(v) },
+          })}
           defaultValue={keywords && keywords[1]}
           placeholder="두 번째 키워드"
+          // 툴팁 설정
+          data-tooltip-id="keyword2"
+          data-tooltip-content="공백은 입력할 수 없습니다."
+          data-tooltip-place="bottom"
         />
       </S.FormInputWrapper>
       <S.FormBtn variant="contained" size="MEDIUM" fontSize="LARGE">
         검색
       </S.FormBtn>
+
+      {errors.keyword1?.type === 'noSpace' && <Tooltip id="keyword1" className="tooltip" isOpen />}
+      {errors.keyword2?.type === 'noSpace' && <Tooltip id="keyword2" className="tooltip" isOpen />}
     </S.Form>
   );
 };
--- frontend/src/pages/YoutubeMainPage/index.tsx
+++ frontend/src/pages/YoutubeMainPage/index.tsx
@@ -1,28 +1,10 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { SearchBar } from '@/components/molecules';
 import * as S from './index.styles';
-import { ROUTER_PATH } from '@/constants/path';
 
 const YoutubeMainPage = () => {
-  const [value, setValue] = useState('');
-  const navi = useNavigate();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    navi(`/${ROUTER_PATH.YOUTUBE_RESULT_PAGE}`, { state: { link: value } });
-  };
   return (
     <S.Wrapper>
-      <SearchBar
-        placeholder="유튜브 링크를 입력하세요."
-        value={value}
-        onChange={handleChange}
-        onSubmit={handleSubmit}
-      />
+      <SearchBar placeholder="유튜브 링크를 입력하세요." />
     </S.Wrapper>
   );
 };
