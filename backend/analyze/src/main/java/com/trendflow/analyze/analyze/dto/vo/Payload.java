6dcba2484b2628cc4349a255fdce7b6711e82665
tableMinPark <tablemin_park@daum.net>
2023-04-05 20:20:59 +0900

    Test : findRelationContent keyword

--- backend/analyze/src/main/java/com/trendflow/analyze/analyze/dto/vo/Payload.java
+++ backend/analyze/src/main/java/com/trendflow/analyze/analyze/dto/vo/Payload.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Payload {
 	@JsonProperty("comment_df")
