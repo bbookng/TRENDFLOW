757a9dff349b30a36fe352a86359df7edca7542a
tjrghks96 <tjrghks96@naver.com>
2023-04-07 09:58:47 +0900

    Fix

--- frontend/src/components/organisms/YoutubeResult/CommentAffinity/index.tsx
+++ frontend/src/components/organisms/YoutubeResult/CommentAffinity/index.tsx
@@ -18,61 +18,22 @@ const CommentAffinity = ({ positive, negative, neutral }: AffinityProps): React.
       <S.Title>댓글 선호도 분석</S.Title>
       <S.AffinityPaper>
         <S.BarChart>
-          <S.BarItem
-            kind="positive"
-            value={convertPercentage(
-              positive as number,
-              (positive as number) + (negative as number) + (neutral as number)
-            )}
-          ></S.BarItem>
-          <S.BarItem
-            kind="negative"
-            value={convertPercentage(
-              negative as number,
-              (positive as number) + (negative as number) + (neutral as number)
-            )}
-          ></S.BarItem>
-          <S.BarItem
-            kind="neutral"
-            value={convertPercentage(
-              neutral as number,
-              (positive as number) + (negative as number) + (neutral as number)
-            )}
-          ></S.BarItem>
+          <S.BarItem kind="positive" value={positive}></S.BarItem>
+          <S.BarItem kind="negative" value={negative}></S.BarItem>
+          <S.BarItem kind="neutral" value={neutral}></S.BarItem>
         </S.BarChart>
         <S.ChartLabels>
           <S.ChartLabel kind="positive">
             <S.Circle kind="positive"></S.Circle>
-            <S.Span>
-              긍정{' '}
-              {convertPercentage(
-                positive as number,
-                (positive as number) + (negative as number) + (neutral as number)
-              )}
-              %
-            </S.Span>
+            <S.Span>긍정 {positive}%</S.Span>
           </S.ChartLabel>
           <S.ChartLabel kind="negative">
             <S.Circle kind="negative"></S.Circle>
-            <S.Span>
-              부정{' '}
-              {convertPercentage(
-                negative as number,
-                (positive as number) + (negative as number) + (neutral as number)
-              )}
-              %
-            </S.Span>
+            <S.Span>부정 {negative}%</S.Span>
           </S.ChartLabel>
           <S.ChartLabel kind="neutral">
             <S.Circle kind="neutral"></S.Circle>
-            <S.Span>
-              중립{' '}
-              {convertPercentage(
-                neutral as number,
-                (positive as number) + (negative as number) + (neutral as number)
-              )}
-              %
-            </S.Span>
+            <S.Span>중립 {neutral}%</S.Span>
           </S.ChartLabel>
         </S.ChartLabels>
       </S.AffinityPaper>
